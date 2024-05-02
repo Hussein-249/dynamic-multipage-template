2924a57d383b68b5b0ca1acf48698526b4424ee9
Hussein-249 <husseinadi8@gmail.com>
2024-05-02 16:24:45 -0400

    Added views, updated server code.

--- server.js
+++ server.js
@@ -11,11 +11,22 @@ const PORT = process.env.PORT || 3001;
 
 var app = express();
 
+app.set('view engine', 'ejs'); // using the express ejs view engine
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    const showDiv = true; // for toggling divs in templates
+    const myvar = "Dynamic Var";
+    res.render('index', { showDiv, myvar });
+
+});
+
 app.listen(PORT, error => { 
     if (error) { console.log(error); }
     else {
         console.log(`Server has been started. Listening on port: ${PORT}`);
         console.log(`Ctrl + C to terminate batch job.`);
-        console.log(`Viewable at:`); // include later
+        console.log(`Viewable at: http://localhost:3001/'`); // include later
     }
 });
