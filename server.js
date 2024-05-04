62d794b6751dc238446899d7fec08e340af3373c
Hussein-249 <husseinadi8@gmail.com>
2024-05-03 21:43:13 -0400

    Added partials, new packages, and bootstrap.

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 /*
-Very basic express server.
+Very basic Express server.
 
 Server listens on port 3001 unless specified in environment variables.
 */
-var http = require('http');
-var express = require('express');
-var path = require('path');
+const http = require('http');
+const express = require('express');
+
+// import mime from 'mime'; // to correctly set MIME types
+const path = require('path');
+const redis = require('redis'); // for a shared cache when re-using content, implement later
 
 const PORT = process.env.PORT || 3001;
 
-var app = express();
+const app = express();
+
+const redisClient = redis.createClient();
 
 app.set('view engine', 'ejs'); // using the express ejs view engine
 
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// there may be a source map warning associated with this
+app.get('/node_modules/bootstrap/dist/css/bootstrap.min.css', (req, res) => {
+    const bootstrapPath = path.join(__dirname, '/node_modules/bootstrap/dist/css/bootstrap.min.css');
+    res.setHeader('Content-Type', 'text/css'); // manually ensuring that bootstrap.min.css is recognized properly
+    res.sendFile(bootstrapPath);
+});
 
 app.get('/', (req, res) => {
     const showDiv = true; // for toggling divs in templates
@@ -26,7 +40,7 @@ app.listen(PORT, error => {
     if (error) { console.log(error); }
     else {
         console.log(`Server has been started. Listening on port: ${PORT}`);
-        console.log(`Ctrl + C to terminate batch job.`);
+        console.log(`Ctrl + C to terminate server.`);
         console.log(`Viewable at: http://localhost:3001/'`); // include later
     }
 });
